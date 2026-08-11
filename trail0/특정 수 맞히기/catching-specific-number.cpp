c0af737b9058d97307c2e7756541ce593150a026
hyerin911 <hellin911@naver.com>
2026-08-11 18:19:14 +0900

    [Passed] 특정 수 맞히기 (2ms, 0MB)

---
+++ trail0/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int num;
+    cin>>num;
+
+    while(num!=25){
+        if(num<25){
+            cout<<"Higher\n";
+            cin>>num;
+        }
+        else{
+            cout<<"Lower\n";
+            cin>>num;
+        }
+    }
+
+    cout<<"Good";
+
+    return 0;
+}
