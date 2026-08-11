cad16264d3806d56bb5208db0e69261fb26492c6
hyerin911 <hellin911@naver.com>
2026-08-12 00:09:24 +0900

    [Passed] 배열의 합 (2ms, 0MB)

---
+++ trail0/배열의 합/sum-of-array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int matrix[4][4];
+
+    for(int i=0; i<4; i++){
+        for(int j=0; j<4; j++){
+            cin>>matrix[i][j];
+        }
+    }
+
+    for(int i=0; i<4; i++){
+        int sum=0;
+        for(int j=0; j<4; j++){
+            sum+=matrix[i][j];
+        }
+        cout<<sum<<"\n";
+    }
+    return 0;
+}
