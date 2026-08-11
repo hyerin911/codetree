d6879861868366d30953266060b5cd01b38576b3
hyerin911 <hellin911@naver.com>
2026-08-11 15:40:37 +0900

    [Passed] B부터 A까지 감소 (2ms, 0MB)

---
+++ trail0/B부터 A까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+
+    cin>>A;
+    cin>>B;
+
+    for(int i=B; i>=A; i--){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
