d4b68a574a5874f4c26b16085e6c742d8de8aa09
hyerin911 <hellin911@naver.com>
2026-08-11 15:09:46 +0900

    [Passed] 음수 구별하기 (1ms, 0MB)

---
+++ trail0/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin>>N;
+    
+    cout<<N;
+    
+    if(N<0){
+        cout<<"\nminus";
+    }
+
+    return 0;
+}
