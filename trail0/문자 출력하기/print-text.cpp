2519596fe45cd622940676d161c060a735878989
hyerin911 <hellin911@naver.com>
2026-08-11 15:29:44 +0900

    [Passed] 문자 출력하기 (2ms, 0MB)

---
+++ trail0/문자 출력하기/print-text.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char MOJI;
+
+    cin>>MOJI;
+
+    for(int i=0; i<8; i++){
+        cout<<MOJI;
+    }
+    
+    return 0;
+}
