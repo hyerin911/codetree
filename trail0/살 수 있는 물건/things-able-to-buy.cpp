0df403feac39a34877d0ea67dad7dd82cdd3a14c
hyerin911 <hellin911@naver.com>
2026-08-11 15:26:08 +0900

    [Passed] 살 수 있는 물건 (2ms, 0MB)

---
+++ trail0/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin>>N;
+    
+    if(N>=3000){
+        cout<<"book";
+    }
+    else if(N>=1000){
+        cout<<"mask";
+    }
+    else{
+        cout<<"no";
+    }
+    return 0;
+}
