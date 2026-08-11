683d8529e10711c53df7b670b52dcab153c279d5
hyerin911 <hellin911@naver.com>
2026-08-11 15:24:18 +0900

    [Passed] 물의 상태 (2ms, 0MB)

---
+++ trail0/물의 상태/state-of-water.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+
+    cin>>n;
+    if(n<0){
+        cout<<"ice";
+    }
+    else if(100<=n){
+        cout<<"vapor";
+    }
+    else{
+        cout<<"water";
+    }
+    
+    return 0;
+}
