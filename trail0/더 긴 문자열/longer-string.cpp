5d2698654c1b0adaef84dbe394d3dc51dd1a4474
hyerin911 <hellin911@naver.com>
2026-08-11 17:33:24 +0900

    [Passed] 더 긴 문자열 (2ms, 0MB)

---
+++ trail0/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a, b;
+    cin>>a;
+    cin>>b;
+
+    if(a.length()>b.length()){
+        cout<<a<<" "<<a.length();
+    }
+    else if(a.length()<b.length()){
+        cout<<b<<" "<<b.length();
+    }
+    else{
+        cout<<"same";
+    }
+
+    return 0;
+}
