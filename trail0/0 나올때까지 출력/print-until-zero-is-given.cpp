38f7747e2768e6e69efa556610fab1d83506681c
hyerin911 <hellin911@naver.com>
2026-08-11 18:13:50 +0900

    [Passed] 0 나올때까지 출력 (1ms, 0MB)

---
+++ trail0/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int num;
+
+    cin>>num;
+    while(num!=0){
+        cout<<num<<"\n";
+        cin>>num;
+    }
+    return 0;
+}
