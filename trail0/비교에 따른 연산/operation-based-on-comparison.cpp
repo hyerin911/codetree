855b79b8007e0518ac46231421db97ebbaaef846
hyerin911 <hellin911@naver.com>
2026-08-11 15:17:22 +0900

    [Passed] 비교에 따른 연산 (2ms, 0MB)

---
+++ trail0/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+
+    cin>>a;
+    cin>>b;
+
+    if(a>b){
+        cout<<a*b;
+    }
+    else{
+        cout<<b/a;
+    }
+    return 0;
+}
