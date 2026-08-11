8b941dde67a0be2c09152450da24829d80d5f0e4
hyerin911 <hellin911@naver.com>
2026-08-11 14:06:27 +0900

    [Passed] 입력받은 값과 합 출력 (2ms, 0MB)

---
+++ trail0/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin>>A;
+    cin>>B;
+
+    cout<<A<<" "<<B<<" "<<A+B;
+    // Please write your code here.
+    return 0;
+}
