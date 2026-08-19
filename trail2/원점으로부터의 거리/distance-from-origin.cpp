e99852e06951555f6abd0c64ebb166510eacee7c
hyerin911 <hellin911@naver.com>
2026-08-19 10:19:56 +0900

    [Passed] 원점으로부터의 거리 (2ms, 0MB)

---
+++ trail2/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <utility>
+
+#define MAXN 1000
+
+using namespace std;
+
+int GetDistFromOrigin(int x, int y){
+    return abs(x)+abs(y);
+}
+
+pair<int, int> distances[MAXN]; //pair<int, int> distances[MAXN];
+
+int main(){
+    int n;
+    cin>>n;
+
+    int x, y;
+    for(int i=0; i<n; i++){
+        cin>>x>>y;
+        distances[i]=make_pair(GetDistFromOrigin(x, y), i+1); //make_pair
+    }
+
+    sort(distances, distances+n);
+
+    for(int i=0; i<n; i++)
+        cout<<distances[i].second<<endl; //second는 y
+    
+    return 0;
+}
+
