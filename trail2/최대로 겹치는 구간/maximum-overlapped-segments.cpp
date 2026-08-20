f61c386fdd158bc4cfec4be652e9f38db259ca43
hyerin911 <hellin911@naver.com>
2026-08-20 18:54:49 +0900

    [Passed] 최대로 겹치는 구간 (2ms, 0MB)

---
+++ trail2/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#define MAX_N 100
+#define MAX_R 200
+#define OFFSET 100
+
+using namespace std;
+
+int n;
+int x1[MAX_N], x2[MAX_N];
+
+int checked[MAX_R+1];
+
+int main(){
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>x1[i]>>x2[i];\
+        
+        x1[i]+=OFFSET;
+        x2[i]+=OFFSET;
+    }
+
+    for(int i=0; i<n; i++)
+        for(int j=x1[i]; j<x2[i]; j++)
+            checked[j]++;
+
+    int max=0;
+    for(int i=0; i<MAX_R; i++)
+        if(checked[i]>max)
+            max=checked[i];
+
+    cout<<max<<"\n";
+    return 0;
+}
